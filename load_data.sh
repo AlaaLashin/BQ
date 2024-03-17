4668acc6888d34fbe54da119cae94fce831c52d7
AlaaLashin <163746466+AlaaLashin@users.noreply.github.com>
2024-03-17 22:27:42 +0200

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -1,9 +1,6 @@
+
 PROJECT_ID='valeo-cp0512-acp3'
 DATASET_ID='Config'
 TABLE_ID='configurations1'
-
-bq ls --transfer_config --transfer_location=eu --format=prettyjson "$PROJECT_ID" | \
-jq -c '.[] | select(.dataSourceId == "scheduled_query") | {schedule, displayName, destinationDatasetId, name, "params": { "destination_table_name_template": .params.destination_table_name_template, "write_disposition": .params.write_disposition }}' | \
-jq -c '. | "INSERT INTO `${DATASET_ID}.${TABLE_ID}` JSON '"'"'[\(.)]'"'"' " | \
-bq query --use_legacy_sql=false
-  
+bq ls --transfer_config --transfer_location=eu --format=prettyjson "$PROJECT_ID" | jq -c '.[] | select(.dataSourceId == "scheduled_query") | {schedule, displayName, destinationDatasetId, name, "params": { "destination_table_name_template": .params.destination_table_name_template, "write_disposition": .params.write_disposition }}' > transfer_configs_filtered_1.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON "${DATASET_ID}.${TABLE_ID}" transfer_configs_filtered_1.json
